bcc0cad2e6b09b0f29c8f9917b7fc14741f86fa0
Nali <nalikante@gmail.com>
2021-09-12 18:15:55 +0200

    Add date piecker

--- src/components/features/OrderOption/OrderOptionDate.js
+++ src/components/features/OrderOption/OrderOptionDate.js
@@ -2,17 +2,13 @@ import React from 'react';
 import styles from './OrderOption.scss';
 import PropTypes from 'prop-types';
 import DatePicker from 'react-datepicker';
-
 import 'react-datepicker/dist/react-datepicker.css';
 
 export const OrderOptionDate = ({currentValue, setOptionValue}) => (
-  <div>
+  <div className={styles.input}>
     <DatePicker
-      className ={styles.input}
-      value ={currentValue}
       selected ={currentValue}
       onChange = {setOptionValue}>
-        Choose a date
     </DatePicker>
   </div>
 );
