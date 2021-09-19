8ceedfe2bd4fd9f169b30e41362938a2f5e06578
Nali <nalikante@gmail.com>
2021-09-19 09:42:16 +0200

    Resolved issue wth tripId Name and CountryCode

--- src/components/features/OrderOption/OrderOptionDate.js
+++ src/components/features/OrderOption/OrderOptionDate.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 
+
 export const OrderOptionDate = ({currentValue, setOptionValue}) => (
   <div className={styles.input}>
     <DatePicker
